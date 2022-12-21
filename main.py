c7e083e8a8baccd3c508f9d0a07ffe90e4b02584
Kenny Fitzgerald <40469281+kennyfitzgerald@users.noreply.github.com>
2022-12-21 06:59:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from rightmoveScraper import *
 import os
 
-url = 'https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION%5E87503&maxBedrooms=3&minBedrooms=3&radius=3.0&propertyTypes=&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=&keywords='
-max_price_pp = 900
+url = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=STATION%5E10022&insId=1&radius=1.0&minPrice=&maxPrice=1750&minBedrooms=1&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='
+max_price_pp = 1000
 
 sender = os.environ.get('EMAIL_SENDER')
 receivers = os.environ.get('EMAIL_RECEIVERS')
