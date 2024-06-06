f820c45e672235752b973838ef5b70dbb6115f7e
kenny <kennyafitzgerald@gmail.com>
2024-06-06 19:34:00 +0100

    remove

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from telegram import *
 from google_sheet import *
 import os
 
-TELEGRAM_API_KEY = '7023504148:AAEfzAAapP2W41Q0_rSeX5IOaovkhGCJY7w'
-GSHEETS_RIGHTMOVE_SHEET_ID = '1PVl4iOOuNSwYjAHw1YYOjd9C0LXyH7kcI1ugmXOpp1I'
+TELEGRAM_API_KEY = os.environ.get("TELEGRAM_API_KEY")
+GSHEETS_RIGHTMOVE_SHEET_ID = os.environ.get("GSHEETS_RIGHTMOVE_SHEET_ID")
 GSHEETS_GID = 0
 
 configs = google_sheet_to_json(GSHEETS_RIGHTMOVE_SHEET_ID, GSHEETS_GID)
